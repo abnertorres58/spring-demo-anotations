61157b2ede216b331ad241e43d46e69be4237e33
Fred <fred@Freds-MacBook-Pro.local>
2020-01-29 16:52:41 -0500

    getting the default bean of a class using the first letter in lowerCase

--- src/com/abnertorres/springdemo/TennisCoach.java
+++ src/com/abnertorres/springdemo/TennisCoach.java
@@ -2,7 +2,7 @@ package com.abnertorres.springdemo;
 
 import org.springframework.stereotype.Component;
 
-@Component("thatSillyCoach")
+@Component
 public class TennisCoach implements Coach {
 
     @Override
