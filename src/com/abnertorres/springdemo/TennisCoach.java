9cf7dcedf669c2c3c508b7e50e26eb06c59a5022
Fred <fred@Freds-MacBook-Pro.local>
2020-01-31 11:12:02 -0500

    Field injection (Not so recomended)

--- src/com/abnertorres/springdemo/TennisCoach.java
+++ src/com/abnertorres/springdemo/TennisCoach.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class TennisCoach implements Coach {
 
+    @Autowired
     private FortuneService fortuneService;
 
     //define a default constructor
@@ -18,11 +19,11 @@ public class TennisCoach implements Coach {
 //        this.fortuneService = fortuneService;
 //    }
 
-    @Autowired
-    public void doSomeCrazyStuff(FortuneService fortuneService) {
-        System.out.println(">> TennisCoach: inside  doSomeCrazyStuff method");
-        this.fortuneService = fortuneService;
-    }
+//    @Autowired
+//    public void doSomeCrazyStuff(FortuneService fortuneService) {
+//        System.out.println(">> TennisCoach: inside  doSomeCrazyStuff method");
+//        this.fortuneService = fortuneService;
+//    }
 
     @Override
     public String getDailyWorkout() {
